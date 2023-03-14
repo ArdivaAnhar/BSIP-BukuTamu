2ef2d4cd8b01cb6446ee258e074b7879806cb345
anata_mee <108978057+ArdivaAnhar@users.noreply.github.com>
2023-03-14 14:28:34 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server = "localhost";
-$user = "root";
-$password = "";
-$database = "dbbukutamu";
-
-$koneksi = mysqli_connect($server, $user, $password, $database) or die(mysqli_error($koneksi));
-?>
